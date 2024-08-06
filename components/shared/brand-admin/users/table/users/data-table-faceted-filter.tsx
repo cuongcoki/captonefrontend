7d67d6956d7e8f10391c19af13e90a3d1b4d7499
ImGib <trangiabao5102@gmail.com>
2024-08-07 00:39:19 +0700

    merge from main

--- components/shared/brand-admin/users/table/users/data-table-faceted-filter.tsx
+++ components/shared/brand-admin/users/table/users/data-table-faceted-filter.tsx
@@ -83,7 +83,7 @@ export function DataTableFacetedFilter<TData, TValue>({
         <Command>
           <CommandInput placeholder={title} />
           <CommandList>
-            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandEmpty>Không có vai trò.</CommandEmpty>
             <CommandGroup>
               {options.map((option) => {
                 const isSelected = selectedValues.has(option.value)
