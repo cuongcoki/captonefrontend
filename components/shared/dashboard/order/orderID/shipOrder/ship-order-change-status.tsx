5b738991d164400dc047690ca105e55a9a62f5b6
Vinaz <125428096+vinh23052002@users.noreply.github.com>
2024-08-16 01:40:29 +0700

    Fix Rerender order shipment

---
+++ components/shared/dashboard/order/orderID/shipOrder/ship-order-change-status.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import React, { ReactElement, ReactNode } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { ShipOrder } from "@/components/shared/dashboard/order/orderID/shipOrder/ShipOrder";
+export default function ShipOrderChangeStatus({
+  children,
+  item,
+  handleSubmitOrderStatus,
+}: {
+  children: ReactElement;
+  item: ShipOrder;
+  handleSubmitOrderStatus: (id: string) => void;
+}): ReactElement {
+  const [open, setOpen] = React.useState(false);
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant="outline"> {item.statusDescription}</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Đổi trạng thái đơn hàng</DialogTitle>
+        </DialogHeader>
+        {children}
+        <DialogFooter>
+          <Button
+            type="submit"
+            onClick={() => {
+              handleSubmitOrderStatus(item.shipOrderId);
+              setOpen(false);
+            }}
+          >
+            Lưu thay đổi
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
