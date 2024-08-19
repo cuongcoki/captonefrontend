cacdf611d00cc2e453fa0a39dc2161cdf0ff7bcd
C_CoKi <trandinhcuongfpt2022@gmail.com>
2024-08-19 23:03:00 +0700

    fix mobile add user và darkmode

--- components/ui/my-drawer.tsx
+++ components/ui/my-drawer.tsx
@@ -10,7 +10,16 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
-import { Minus, Plus } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Badge } from "@/components/ui/badge";
+import { Minus, MoonIcon, Plus, SunIcon } from "lucide-react";
 import { Bar, BarChart, ResponsiveContainer } from "recharts";
 import SideNavbar from "../shared/dashboard/sidebar/SideNavbar";
 import { LogoIcon } from "@/constants/images";
@@ -24,12 +33,36 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { authApi } from "@/apis/auth.api";
 import toast from "react-hot-toast";
-
+import { useTheme } from "next-themes";
+import Link from "next/link";
+export const Role = [
+  {
+    value: 1,
+    label: "Quản lý hệ thống",
+  },
+  {
+    value: 2,
+    label: "Quản lý cơ sở",
+  },
+  {
+    value: 3,
+    label: "Quản lý số lượng",
+  },
+  {
+    value: 4,
+    label: "Nhân viên vận chuyển",
+  },
+  {
+    value: 5,
+    label: "Nhân viên",
+  },
+];
 export function MyDrawer() {
   const [loading, setLoading] = useState<boolean>(false);
   // ** hooks
   const user = useAuth();
   const router = useRouter();
+  const { setTheme } = useTheme();
 
   const handleLogout = () => {
     setLoading(true);
@@ -64,11 +97,13 @@ export function MyDrawer() {
         />
       </DrawerTrigger>
       <DrawerContent>
-        <div className="mx-auto w-full ">
-          <DrawerHeader>
-            <DrawerTitle>Tiến Huy</DrawerTitle>
-            <DrawerDescription>Công ty mây tre Tiến Huy</DrawerDescription>
-          </DrawerHeader>
+        <div className="mx-auto w-full relative">
+          <Link href={`/profile/${user.user?.id}`}>
+            <DrawerHeader>
+              <DrawerTitle className="text-primary">{user.user?.lastName}{user.user?.firstName}</DrawerTitle>
+              <DrawerDescription> {Role.find((role) => role.value === user?.user?.roleId)?.label}</DrawerDescription>
+            </DrawerHeader>
+          </Link>
           <div className="p-4 pb-0">
             <BottomNavbar />
           </div>
@@ -79,6 +114,30 @@ export function MyDrawer() {
             <Button variant="outline" onClick={handleLogout}>
               Đăng xuất
             </Button>
+            <Badge
+              variant="secondary"
+              className="absolute top-0 text-xs cursor-pointer hover:bg-gray-200 border p-2 shadow-md "
+            >
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <div className="w-full flex items-center ">
+                    <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                  </div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="start">
+                  <DropdownMenuItem onClick={() => setTheme("light")}>
+                    Sáng
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setTheme("dark")}>
+                    Tối
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setTheme("system")}>
+                    Hệ thống
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </Badge>
           </DrawerFooter>
         </div>
       </DrawerContent>
